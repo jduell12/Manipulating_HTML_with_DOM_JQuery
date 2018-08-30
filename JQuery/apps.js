5310763efae9602745b2a9e76d4f4d6fca712571
Jessica Duell <duellrdh@gmail.com>
2018-08-29 23:21:07 -0400

    change

--- JQuery/apps.js
+++ JQuery/apps.js
@@ -1,6 +1,6 @@
 //Checks to see HTML has loaded before running
 $(document).ready(function(){
-    
+
     //Creating a div element and appending it to the body
     $("body").append("<div></div>");
 
