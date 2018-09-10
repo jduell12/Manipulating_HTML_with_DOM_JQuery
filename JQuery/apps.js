a1f47a58a3b5e3b8858f7e432978aff7a9552d88
Jessica Duell <duellrdh@gmail.com>
2018-09-09 22:19:10 -0400

    Completed objective 1

--- JQuery/apps.js
+++ JQuery/apps.js
@@ -2,7 +2,24 @@
 $(document).ready(function(){
 
     //Creating a div element and appending it to the body
-    $("body").append("<div></div>");
+    let div = document.createElement("div");
+    $("body").append(div);
+    
+    //Creating an ordered list and appending it to the div
+    let ol = document.createElement("ol");
+    $("div").append(ol);
 
+    //Creating the first list item
+    let li1 = document.createElement("li");
+    $("ol").append(li1);
+    //Make button appear 
+    let button1 = document.createElement("button");
+    let btnTxt = document.createTextNode("Click Me");
+    $(button1).append(btnTxt);
+    $(li1).append(button1);
+    //When button is clicked a nice message with appear
+    $(button1).click(function(){
+        alert("Have a good day!");
+    });
 
 });
